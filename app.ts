8c982a8f15b925a5f2805be6d6a835d202dae39a
unknown <Uchechiiwuh12@icloud.com>
2024-09-27 16:42:19 +0100

    first commit

---
+++ app.ts
@@ -0,0 +1,82 @@
+require("dotenv").config();
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { ErrorMiddleware } from "./middleware/error";
+import userRouter from "./routes/user.route";
+import courseRouter from "./routes/course.route";
+import orderRouter from "./routes/order.route";
+import notificationRouter from "./routes/notification.route";
+import analyticsRouter from "./routes/analytics.route";
+import layoutRouter from "./routes/layout.route";
+import { rateLimit } from "express-rate-limit";
+import {v2 as cloudinary} from "cloudinary";
+import http from "http";
+import connectDB from "./utils/db";
+import { initSocketServer } from "./socketServer";
+
+const app = express();
+const server = http.createServer(app);
+
+// body parser
+app.use(express.json({ limit: "50mb" }));
+
+// cookie parser
+app.use(cookieParser());
+
+// cors => cross origin resource sharing
+app.use(cors());
+
+// api requests limit
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: "draft-7",
+  legacyHeaders: false,
+});
+
+// routes
+app.use(
+  "/api/v1",
+  userRouter,
+  orderRouter,
+  courseRouter,
+  notificationRouter,
+  analyticsRouter,
+  layoutRouter
+);
+
+// testing api
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    succcess: true,
+    message: "API is working",
+  });
+});
+
+// unknown route
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
+
+// middleware calls
+app.use(limiter);
+app.use(ErrorMiddleware);
+
+
+// cloudinary config
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_SECRET_KEY,
+ });
+ 
+ initSocketServer(server);
+ 
+ // create server
+ server.listen(Number(process.env.PORT) || 8000, '0.0.0.0', () => {
+     console.log(`Server is connected with port ${process.env.PORT}`);
+     connectDB();
+ });
